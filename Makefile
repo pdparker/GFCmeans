07c6f8ce23d55919e4108541bd6fb5b7e46e9ceb
Philip Parker <philip.parker@acu.edu.au>
2015-02-24 07:08:29 +1100

    Added survey means for 1995

--- Makefile
+++ Makefile
@@ -2,18 +2,20 @@
 
 .SUFFIXES: .csv .R
 
-all: f1995.csv f1998.csv f2003.csv f2006.csv
+all: f1995.csv f1998.csv f2003.csv f2006.csv \
+	m1995.csv m1998.csv m2003.csv m2006.csv \
+	s1995.csv
 
-f1995.csv: listWise1995.R
+s1995.csv m1995.csv f1995.csv: listWise1995.R
 	/usr/bin/Rscript listWise1995.R
 
-f1998.csv: listWise1998.R
+m1998.csv f1998.csv: listWise1998.R
 	/usr/bin/Rscript listWise1998.R
 
-f2003.csv: listWise2003.R
+m2003.csv f2003.csv: listWise2003.R
 	/usr/bin/Rscript listWise2003.R
 
-f2006.csv: listWise2006.R
+m2006.csv f2006.csv: listWise2006.R
 	/usr/bin/Rscript listWise2006.R
 
 clean:
--- listWise1995.R
+++ listWise1995.R
@@ -1,7 +1,12 @@
-### Data Prep 1995 ###
+								### Data Prep 1995 ###
 
 ## Package Load
 library(RSQLite)
+library(magrittr)
+library(survey)
+library(Amelia)
+library(mitools)
+library(parallel)
 #-------------------------------------------------
 
 ## Extract data from SQL database
@@ -16,10 +21,16 @@ db <- dbConnect(SQLite(), dbname="~/Dropbox/Databases/SQLdb/LSAY2015_update.db")
 	# Exclusion of ecopnomy and politics questions as not consistently present
 	lsTags <- lsTags[-c(grep("country|economy", lsTags$Data.element)),]
 
-	# Extraction of major data
-	query <- paste("SELECT ", paste(lsTags$Variable, collapse=","),
-				   " FROM LSAY1995")
+	# Extract Weight  variables
+	weightTags <- m1995[grep("WT[0-9]{4}$| $", m1995$Variable),]
+	# Remove weight for 1996
+	weightTags %<>% "["(-1,)
+
+	# Extraction of major data from database
+	query <- paste("SELECT ", paste(lsTags$Variable, collapse=","), ",", 
+				   paste(weightTags$Variable, collapse=","), " FROM LSAY1995")
 	d1995 <- dbGetQuery(db, query)
+	# Add id
 	d1995$id <- paste0("C95.",row.names(d1995))
 #-------------------------------------------------
 
@@ -33,6 +44,53 @@ source("./library/editRules.R")
 	# Run frequencies
 	F1995 <- frequencyFun(d1995[,1:120])
 	if(dim(F1995)==c(120,4)) {write.csv(F1995, file = "f1995.csv")}
-## Get means
-M1995 <- apply(d1995[,1:120],2, mean, na.rm=TRUE)
-write.csv(M1995, file = "m1995.csv")
+	# Get means
+	M1995 <- apply(d1995[,1:120],2, mean, na.rm=TRUE)
+	write.csv(M1995, file = "m1995.csv")
+#-------------------------------------------------
+
+## Survey package - Imputation and Replicant Weights
+	# Reorder
+	d1995 <- d1995[,c(order(names(d1995)[1:120]), 121:131)]
+	names(d1995) <- gsub("(|\\.)[0-9]+", "", names(d1995))
+	# Rename weight variables
+	# Stack data
+	dLong <- rbind.data.frame(d1995[,c(1,11,21,31,41,51,61,71,81,91,101,111,121,131)], 
+							  d1995[,c(2,12,22,32,42,52,62,72,82,92,102,112,122,131)],
+							  d1995[,c(3,12,23,33,43,53,63,73,83,93,103,113,123,131)], 
+							  d1995[,c(4,14,24,34,44,54,64,74,84,94,104,114,124,131)],
+							  d1995[,c(5,15,25,35,45,55,65,75,85,95,105,115,125,131)],
+							  d1995[,c(6,16,26,36,46,56,66,76,86,96,106,116,126,131)],
+							  d1995[,c(7,17,27,37,47,57,67,77,87,97,107,117,127,131)],
+							  d1995[,c(8,18,28,38,48,58,68,78,88,98,108,118,128,131)],
+							  d1995[,c(9,19,29,39,49,59,69,79,89,99,109,119,129,131)],
+							  d1995[,c(10,20,30,40,50,60,70,80,90,100,110,120,130,131)])
+	dLong$trend <- rep(1:10, each=(nrow(d1995)))
+	# List wise delet by attrition
+	dLong <- dLong[!is.na(dLong$WT),]
+	# Check number of complete cases
+	sum(complete.cases(dLong))
+	#Multiple Imputations of data holes
+	MI <- amelia(dLong,m = 5, p2s = 1, idvars = c('WT', 'id'))
+	a <- imputationList(MI$imputations)
+	rm(list=c("dLong", "d1995", "lsTags", "weightTags", "F1995", "m1995"))
+	# Wrap in srvey package
+	dclust <- svydesign(ids = ~id, weights = ~WT, data = a)
+	rm(a)
+
+## Analysis of weighted data
+	# Set parallel options
+	options(mc.cores=detectCores())
+	# Run Means
+	trendMeans <- MIcombine(with (dclust, 
+							 	 svyby(~general+living+home+future+career+work+
+							  	  	money+lesiure+location+social+people+
+							  	  	independence, 
+							  		  ~trend, svymean, na.rm=TRUE, multicore=TRUE
+							 	 	   )
+								  )
+							)
+	# Wrap means and CIs into table and write to output
+	S1995 <- cbind(trendMeans$coefficients, confint(trendMeans))
+	colnames(S1995)[1] <- "means"
+	write.csv(S1995, file = "s1995.csv")
